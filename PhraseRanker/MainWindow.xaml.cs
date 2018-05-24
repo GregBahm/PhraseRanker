f4b4c6fb1cb38a43182a8087479dd6c55ed6fe6a
derp derp <workingbahm@yahoo.com.sg>
2018-05-24 02:53:25 -0700

    A bug in the progression of the sorting algorythm but otherwise working.

--- PhraseRanker/MainWindow.xaml.cs
+++ PhraseRanker/MainWindow.xaml.cs
@@ -15,14 +15,35 @@ using System.Windows.Shapes;
 
 namespace PhraseFighter
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            string xmlPath = @"C:\Users\Lisa\Documents\PhraseRanker\Prose.xml";
+            DataContext = MainViewModel.LoadFromXml(xmlPath);
+        }
+
+        private MainViewModel LoadFromRaw(string xmlPath)
+        {
+            string rawPath = @"C:\Users\Lisa\Documents\PhraseRanker\Prose.txt";
+            List<SignitureItem> initialItems = SignitureItem.LoadFromRawText(rawPath);
+            return new MainViewModel(initialItems, xmlPath);
+        }
+
+        private void OnChoseRight(object sender, RoutedEventArgs e)
+        {
+            MainViewModel context = (MainViewModel)DataContext;
+            context.GrantRightWin();
+        }
+
+        private void OnChoseLeft(object sender, RoutedEventArgs e)
+        {
+            MainViewModel context = (MainViewModel)DataContext;
+            context.GrantLeftWin();
+        }
     }
-  }
 }
--- PhraseRanker/ViewModel.cs
+++ PhraseRanker/ViewModel.cs
@@ -4,77 +4,143 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace PhraseFighter
 {
-  public abstract class ViewModel : INotifyPropertyChanged
-  {
-    protected void NotifyPropertyChanged(string property)
+    public abstract class ViewModel : INotifyPropertyChanged
     {
-      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-    }
-    public event PropertyChangedEventHandler PropertyChanged;
-  }
-
-  public class EloFight : ViewModel
-  {
-    public SignitureItem LeftItem { get; }
-    public SignitureItem RightItem { get; }
-  }
-
-  public class MainViewModel : ViewModel
-  {
-    private readonly Random _rand;
-    private List<SignitureItem> _items;
-
-    private EloFight _currentFight;
-    public EloFight CurrentFight { get { return _currentFight; } }
-
-    public MainViewModel(IEnumerable<SignitureItem> items)
-    {
-      _rand = new Random();
-      _items = items.ToList();
+        protected void NotifyPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 
-    public void GrantLeftWin()
+    public class RankFight : ViewModel
     {
-      ResolveFight(CurrentFight.LeftItem, CurrentFight.RightItem);
+        public SignitureItem LeftItem { get; }
+        public SignitureItem RightItem { get; }
+
+        public RankFight(SignitureItem leftItem, SignitureItem rightItem)
+        {
+            LeftItem = leftItem;
+            RightItem = rightItem;
+        }
     }
 
-    public void GrantRightWin()
+    public class MainViewModel : ViewModel
     {
-      ResolveFight(CurrentFight.RightItem, CurrentFight.LeftItem);
+        private readonly string _savePath;
+        private int _currentIndex;
+        private int CurrentIndex
+        {
+            get { return _currentIndex; }
+            set
+            {
+                _currentIndex = value % _items.Count;
+            }
+        }
+        private readonly Random _rand;
+        private List<SignitureItem> _items;
+        public IEnumerable<SignitureItem> Items
+        {
+            get { return _items.OrderByDescending(item => item.Rank); }
+        }
+
+        private RankFight _currentFight;
+        public RankFight CurrentFight { get { return _currentFight; } }
+
+        public MainViewModel(IEnumerable<SignitureItem> items, string savePath)
+        {
+            _rand = new Random();
+            _items = items.ToList();
+            _currentFight = GetNewFight();
+            _savePath = savePath;
+        }
+
+        public void GrantLeftWin()
+        {
+            ResolveFight(CurrentFight.LeftItem, CurrentFight.RightItem);
+        }
+
+        public void GrantRightWin()
+        {
+            ResolveFight(CurrentFight.RightItem, CurrentFight.LeftItem);
+        }
+
+        private void ResolveFight(SignitureItem winner, SignitureItem loser)
+        {
+            winner.Rank++;
+            CurrentIndex++;
+            NotifyPropertyChanged(nameof(Items));
+            _currentFight = GetNewFight();
+            NotifyPropertyChanged(nameof(CurrentFight));
+            Save();
+        }
+
+        private RankFight TryGetFight()
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                RankFight ret = GetNewFight();
+                CurrentIndex++;
+                if (ret != null)
+                {
+                    return ret;
+                }
+            }
+            return null;
+        }
+
+        private RankFight GetNewFight()
+        {
+            SignitureItem fighterA = _items[CurrentIndex];
+
+            SignitureItem fighterB = GetFighterB(); 
+            if(fighterB == null)
+            {
+                return null;
+            }
+            return new RankFight(fighterA, fighterB);
+        }
+
+        private SignitureItem GetFighterB()
+        {
+            for (int i = 1; i < _items.Count; i++)
+            {
+                int index = (_currentIndex + i) % _items.Count;
+                if(_items[index].Rank == _items[CurrentIndex].Rank)
+                {
+                    return _items[index];
+                }
+            }
+            return null;
+        }
+
+        void Save()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement rootElement = doc.CreateElement("Root");
+            foreach (SignitureItem item in _items)
+            {
+                rootElement.AppendChild(item.ToXml(doc));
+            }
+            doc.AppendChild(rootElement);
+            doc.Save(_savePath);
+        }
+
+        public static MainViewModel LoadFromXml(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            List<SignitureItem> items = new List<SignitureItem>();
+            foreach (XmlElement item in doc.DocumentElement.ChildNodes)
+            {
+                SignitureItem loadedItem = SignitureItem.FromXml(item);
+                items.Add(loadedItem);
+            }
+            return new MainViewModel(items, path);
+        }
     }
-
-    private void ResolveFight(SignitureItem winner, SignitureItem loser)
-    {
-      int newWinnerElo = (winner.Elo * winner.Challenges + loser.Elo + 400) / (winner.Challenges + 1);
-      int newLoserElo = (loser.Elo * loser.Challenges + loser.Elo + 400) / (loser.Challenges + 1);
-
-      winner.Elo = newWinnerElo;
-      loser.Elo = newLoserElo;
-      winner.Challenges += 1;
-      loser.Challenges += 1;
-
-      _items.Add(winner);
-      _items.Add(loser);
-      GetNewFight();
-    }
-
-    private void GetNewFight()
-    {
-      int minFights = _items.Min(item => item.Challenges);
-      SignitureItem fighterA = _items.First(item => item.Challenges == minFights);
-      _items.Remove(fighterA);
-      int random = _rand.Next(0, _items.Count);
-      SignitureItem challenger = _items[random];
-      _items.RemoveAt(random);
-
-    }
-
-    void Save(string path)
-    {
-      throw new NotImplementedException();
-    }
-  }
 }
